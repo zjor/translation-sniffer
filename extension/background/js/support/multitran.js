f53c31576fed2607679192570fd2e98d9c256ddc
Sergey Royz <sergey.royz@goeuro.com>
2018-09-18 22:37:39 +0200

    first react ui

--- extension/background/js/support/multitran.js
+++ extension/background/js/support/multitran.js
@@ -1,33 +1,28 @@
+import get from '../libs/lodash/get.js'
+import set from '../libs/lodash/set.js'
+
 const multitran = (req) => {
     const resource = "multitran"
     const regex = /.*?:\/\/.*?\.multitran\.ru\/.*/
     if (req.type == "main_frame" && regex.test(req.url)) {
-        const word = /s=(\w*)/.exec(req.url)
-        if (word) {
+        const wordMatcher = /s=([\w\%]*)/.exec(req.url)
+        if (wordMatcher) {
+            //TODO: find out what kind of encoding is used            
+            const word = decodeURIComponent(wordMatcher[1])
+            const lang = "en/ru"
             chrome.storage.local.get([resource], (dictionary) => {
                 const now = new Date()
                 const today = `${now.getFullYear()}-${now.getMonth() + 1}-${now.getDate()}`
-                const value = {
-                    "en/ru": {
-                        [today]: [word[1]]
-                    }
-                }
-                if (dictionary.hasOwnProperty(resource)) {
-                    if (dictionary[resource].hasOwnProperty("en/ru")) {
-                        if (dictionary[resource]["en/ru"].hasOwnProperty(today)) {
-                            dictionary[resource]["en/ru"][today].push(word[1])
-                        } else {
-                            dictionary[resource]["en/ru"][today] = [word[1]]
-                        }
-                    } else {
-                        dictionary[resource]["en/ru"] = {[today]: [word[1]]}
-                    }
-                    chrome.storage.local.set(dictionary, () => console.log(`Updated: ${JSON.stringify(dictionary)}`))
+
+                const list = get(dictionary, [resource, lang, today])
+
+                if (list === undefined) {
+                    set(dictionary, [resource, lang, today], [word])
                 } else {
-                    chrome.storage.local.set({
-                        [resource]: value
-                    }, () => console.log(`Saved: ${resource} -> ${JSON.stringify(value)}`))
+                    list.push(word)
                 }
+                
+                chrome.storage.local.set(dictionary, () => console.log(`Updated: ${JSON.stringify(dictionary)}`))                
             })
         }
         return true
--- src/index.js
+++ src/index.js
@@ -1,8 +1,67 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 
+const data = {
+    "en/ru": {
+        "2018-09-18": [
+            { word: "hello", resource: "multitran" },
+            { word: "world", resource: "multitran" }
+        ]
+    },
+    "cz/ru": {
+        "2018-09-18": [
+            { word: "hezky", resource: "slovnik" },
+            { word: "cesky", resource: "slovnik" }
+        ]
+    }
+}
+
+const Word = ({ word, resource }) => (
+    <div>
+        <span>{word}</span>
+        <span>[{resource}]</span>
+    </div>
+)
+
+const Day = ({ date, words }) => (
+    <div>
+        <h5>{date}</h5>
+        {words.map((word) => <Word key={word.word} word={word.word} resource={word.resource}/>)}
+    </div>
+)
+
+const Language = ({ dates }) => {
+    return (
+        <div>
+            {Object.keys(dates).map((date) => (
+                <div key={date}>
+                    <Day date={date} words={dates[date]}/>
+                </div>
+            ))}
+        </div>
+    )
+}
+
+class History extends Component {
+    render() {
+        return (
+            <div>
+                {Object.keys(data).map((lang) => (
+                    <div key={lang}>
+                        <h3>{lang}</h3>
+                        <Language dates={data[lang]} />
+                    </div>
+                ))}
+            </div>
+        )
+    }
+}
+
 const App = () => (
-    <div>Hello from React</div>
+    <div>
+        <h2>Translation history</h2>
+        <History />
+    </div>
 )
 
 ReactDOM.render(<App />, document.getElementById("root"))
