884c9091528e09f6282f6f0878649505053fc627
Sergey Royz <sergey.royz@goeuro.com>
2018-09-21 18:40:28 +0200

    rendering list of words via react

--- extension/background/js/support/commons.js
+++ extension/background/js/support/commons.js
@@ -3,13 +3,9 @@ import set from '../libs/lodash/set.js'
 
 const storeWord = (lang, date, word) => {
     chrome.storage.local.get([lang], (dictionary) => {
-        const list = get(dictionary, [lang, date])
-
-        if (list === undefined) {
-            set(dictionary, [lang, date], [word])
-        } else {
-            list.push(word)
-        }
+        const list = get(dictionary, [lang, date], [])
+        list.push(word)
+        set(dictionary, [lang, date], list)
         
         chrome.storage.local.set(dictionary, () => console.log(`Updated: ${JSON.stringify(dictionary)}`))         
     })
--- src/index.js
+++ src/index.js
@@ -1,20 +1,102 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 
+import { withStyles } from '@material-ui/core/styles'
+import Card from '@material-ui/core/Card'
+import CardContent from '@material-ui/core/CardContent'
+import Typography from '@material-ui/core/Typography'
+import Table from '@material-ui/core/Table'
+import TableBody from '@material-ui/core/TableBody'
+import TableCell from '@material-ui/core/TableCell'
+import TableHead from '@material-ui/core/TableHead'
+import TableRow from '@material-ui/core/TableRow'
+
+import get from 'lodash/get'
+import set from 'lodash/set'
+
+const styles = {
+    card: {
+      minWidth: 275,
+      marginBottom: '16px'
+    },
+    bullet: {
+      display: 'inline-block',
+      margin: '0 2px',
+      transform: 'scale(0.8)',
+    },
+    pos: {
+      marginBottom: 12,
+    },
+  }
+
 const data = {
     "en/ru": {
         "2018-09-18": [
             { word: "hello", resource: "multitran" },
             { word: "world", resource: "multitran" }
+        ],
+        "2018-09-20": [
+            { word: "allegedly", resource: "multitran" },
+            { word: "perplexity", resource: "multitran" }
         ]
     },
     "cz/ru": {
         "2018-09-18": [
             { word: "hezky", resource: "slovnik" },
             { word: "cesky", resource: "slovnik" }
+        ],
+        "2018-09-21": [
+            { word: "malir", resource: "slovnik" },
+            { word: "tesar", resource: "slovnik" }
         ]
     }
 }
+
+/**
+ * Reorganizes data around dates in the following way:
+ * date -> [{word, lang, resource}]
+ * @param {*} data 
+ */
+const flipData = (data) => {
+    const result = {}
+    Object.keys(data).forEach((lang) => {
+        Object.keys(data[lang]).forEach((date) => {
+            data[lang][date].forEach((word) => {
+                const words = get(result, [date], [])
+                words.push(Object.assign({}, word, { lang }))
+                set(result, [date], words)
+            })
+        })
+    })
+    return result
+}
+
+const SimpleCard = ({ date, words, classes }) => {
+    const bull = <span className={classes.bullet}>•</span>;  
+    return (
+      <Card className={classes.card}>
+        <CardContent>
+          <Typography variant="title">
+            {date}
+          </Typography>
+          <Table>
+              <TableBody>
+                  {words.map((word) => {
+                      return (
+                          <TableRow hover key={word.word}>
+                              <TableCell>{word.word}</TableCell>
+                              <TableCell>{word.lang}</TableCell>
+                              <TableCell>{word.resource}</TableCell>
+                          </TableRow>
+                      )
+                  })}
+              </TableBody>
+          </Table>
+        </CardContent>
+      </Card>
+    );
+  }
+const StyledCard = withStyles(styles)(SimpleCard)
 
 const Word = ({ word, resource }) => (
     <div>
@@ -43,14 +125,25 @@ const Language = ({ dates }) => {
 }
 
 class History extends Component {
-    render() {
+    state = {
+        history: flipData(data)
+    }
+
+    componentDidMount() {
+        if (chrome.storage.local) {
+            chrome.storage.local.get((data) => {
+                this.setState({ history: flipData(data) })
+            })
+        } else {
+            console.log("Running in UI development mode")
+        }
+    }
+
+    render() {        
         return (
             <div>
-                {Object.keys(data).map((lang) => (
-                    <div key={lang}>
-                        <h3>{lang}</h3>
-                        <Language dates={data[lang]} />
-                    </div>
+                {Object.keys(this.state.history).map((date) => (
+                  <StyledCard key={date} date={date} words={this.state.history[date]} />
                 ))}
             </div>
         )
