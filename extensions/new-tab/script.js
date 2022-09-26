f49dca09794551ed13118c4364c2535c4f84933f
Sergey Royz <zjor.se@gmail.com>
2022-09-26 14:41:04 +0200

    store airtable creds locally

--- extensions/new-tab/script.js
+++ extensions/new-tab/script.js
@@ -9,3 +9,27 @@ chrome.identity.getProfileUserInfo((info) => {
 	spanId.innerText = id
 	spanEmail.innerText = email
 })
+
+const configForm = document.querySelector('#airtable_config_form')
+configForm.addEventListener('submit', (e) => {
+	const baseIdInput = document.querySelector('#base_id')
+	const apiKeyInput = document.querySelector('#api_key')
+	const tableIdInput = document.querySelector('#table_id')
+
+	e.preventDefault()
+	chrome.storage.local.set({
+		airtable: {
+			baseId: baseIdInput.value,
+			apiKey: apiKeyInput.value,
+			tableId: tableIdInput.value
+		}
+	}, () => console.log('Airtable config saved'))
+})
+
+chrome.storage.local.get(['airtable'], (result) => {
+	if (result.airtable && result.airtable.baseId) {
+		console.log('Going on')
+	} else {
+		console.log('Airtable config is empty')
+	}
+})
